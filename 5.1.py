483dd833c1b42ad2f54f25b503c3f4384739ef90
whq611 <85942669+whq611@users.noreply.github.com>
2021-07-11 19:49:54 +0800

    Add files via upload

---
+++ 5.1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s):
+        i,j = 0,len(s)-1
+        v = s[0]
+        while i < len(s)-1:
+
+            while i<j:
+                if i == 0:
+                    if s[0] == s[j]:
+                        if s[j::-1] == s[:j+1] and len(s[:j+1]) > len(v):
+                            v = s[:j+1]
+                    j -= 1
+                else:
+                    if s[i] == s[j]:
+                        if s[j:i-1:-1] == s[i:j+1] and len(s[i:j+1]) > len(v):
+                            v = s[i:j+1]
+                    j -= 1
+            i += 1
+            j = len(s)-1
+        return v
