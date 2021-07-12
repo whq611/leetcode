3416675449e694709ec45a8baa6ffc6fcd056e08
whq611 <85942669+whq611@users.noreply.github.com>
2021-07-12 20:11:47 +0800

    Add files via upload

---
+++ 392.(2).py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        n,m = len(s),len(t)
+        f = [[0] * 26 for _ in range(m)]
+        f.append([m] * 26)
+
+        for i in range(m-1,-1,-1):
+            for j in range(26):
+                f[i][j] = i if ord(t[i]) == j + ord('a') else f[i+1][j]
+
+        add = 0
+        for i in range(n):
+            if f[add][ord(s[i]) - ord('a')] == m:
+                      return False
+            add = f[add][ord(s[i]) - ord('a')] + 1
+        return True
