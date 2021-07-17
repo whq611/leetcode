dc520143f58867dde7d6b7e4a75c8fbad2abcc0e
whq611 <85942669+whq611@users.noreply.github.com>
2021-07-17 22:35:55 +0800

    Add files via upload

---
+++ 22.generate paretheses(1)暴力.py
@@ -0,0 +1,28 @@
+class Solution:
+    def generateParentthesis(self,n):
+        def generate(A):
+            if len(A) == 2*n:
+                if valid(A):
+                    ans.append(''.join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            bal = 0
+            for i in A:
+                if i == '(':
+                    bal += 1
+                else:
+                    bal -= 1
+                if bal < 0:
+                    return False
+            return bal == 0
+
+        ans = []
+        generate([])
+        return ans
