31fdada6e837ce3c5875c9b80d0e233e0ff99497
whq611 <85942669+whq611@users.noreply.github.com>
2021-07-26 18:09:43 +0800

    Add files via upload

---
+++ 820.(1).py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumLengthEncoding(self,words):
+        good = set(words)
+        for word in words:
+            for k in range(1,len(words)):
+                good.discard(word[:k])
+        return sum(len(word) + 1 for word in good)
