e33728f186e61ee59bb4e9378c9f13457658c597
whq611 <85942669+whq611@users.noreply.github.com>
2021-08-08 21:23:15 +0800

    Add files via upload

--- 1004.1(滑动窗口).py
+++ 1004.1(滑动窗口).py
@@ -1,7 +1,6 @@
 class Solution:
     def longestOnes(self, nums, k):
-        if k >= nums.count(0):
-            return len(nums)
+
         left = 0
         res = 0
         ans = []
@@ -9,10 +8,9 @@ class Solution:
             if s == 0:
                 ans.append(right)
             if len(ans) == k + 1:
-                if right - left > res:
-                    res = right - left
+
                 left = min(ans) + 1
                 ans.remove(left - 1)
-        if right - left + 1> res:
+            if right - left + 1> res:
                     res = right - left + 1
         return res
