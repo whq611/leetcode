8efe73cc97071cef79d5ad30945d4fbe0b4a54c8
whq611 <85942669+whq611@users.noreply.github.com>
2021-08-23 23:43:30 -0500

    Add files via upload

---
+++ 154.find minimum in rotated sorted array(二分).py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMin(self, nums):
+        low,high = 0,len(nums) - 1
+        while low < high:
+            m = low + (high - low) // 2
+            while nums[m] == nums[high] and high > m:
+                high -= 1
+            if nums[m] <= nums[high]:
+                high = m
+            else:
+                low = m + 1
+        return nums[low]
