36b76adc13bde0f7d48f21b047f5813da278c9fd
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-07 21:52:10 -0500

    Add files via upload

---
+++ 875.(2)二分终极改进.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minEatingSpeed(self, piles, H):
+        def possible(K):
+            return sum((p-1)//K + 1 for p in piles) <= H
+
+        lo,hi = 1,max(piles)
+        while lo < hi:
+            mi = (lo+hi)//2
+            if not possible(mi):
+                lo = mi+1
+            else:
+                hi = mi
+        return lo
