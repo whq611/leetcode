4ebfc213a66c2e2bbe271e52836a347f3e858e2f
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-18 16:30:20 -0500

    Add files via upload

---
+++ 21.双指针改进.py
@@ -0,0 +1,52 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode(None)
+        node = res
+        while l1 and l2:
+            if l1.val<l2.val:
+                node.next,l1 = l1,l1.next
+            else:
+                node.next,l2 = l2,l2.next
+            node = node.next
+        if l1:
+            node.next = l1
+        else:
+            node.next = l2
+        return res.next  
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums1 = [1,2,4]
+    nums2 = [1,3,4]
+    l1 = create_linked_list(nums1)
+    l2 = create_linked_list(nums2)
+    solution = Solution()
+    result = solution.mergeTwoLists(l1,l2)
+    print('结果：')
+    print_linked_list(result)
