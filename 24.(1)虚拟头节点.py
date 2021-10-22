3f6b58702f5622aeca25ee29f716446a1fd00cf8
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-21 19:08:50 -0500

    Add files via upload

---
+++ 24.(1)虚拟头节点.py
@@ -0,0 +1,53 @@
+class ListNode:
+    def __init__(self, x=0,next=None):
+        self.val = x
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head):
+        res = ListNode(next=head)
+        pre = res
+
+        while pre.next and pre.next.next:
+            cur = pre.next
+            post = pre.next.next
+
+            cur.next = post.next
+            post.next = cur
+            pre.next = post
+
+            pre = pre.next.next
+        return res.next
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.swapPairs(head)
+    print('结果：')
+    print_linked_list(result)
