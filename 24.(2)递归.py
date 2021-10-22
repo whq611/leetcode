ca0c7158aa2624508c666fdcbcd419baf42ab5ab
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-21 19:24:51 -0500

    Add files via upload

---
+++ 24.(2)递归.py
@@ -0,0 +1,46 @@
+class ListNode:
+    def __init__(self, x=0,next=None):
+        self.val = x
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head):
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.swapPairs(head)
+    print('结果：')
+    print_linked_list(result)
