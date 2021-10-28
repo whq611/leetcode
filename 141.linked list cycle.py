58988a29a30017d251f329ddaf4ac6309923d3cd
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-28 00:17:30 -0500

    Add files via upload

---
+++ 141.linked list cycle.py
@@ -0,0 +1,49 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head):
+        if not head:
+            return False
+        a = set()
+        a.add(head)
+        while head.next:
+            if head.next not in a:
+                head = head.next
+                a.add(head)
+            else:
+                return True
+        return False
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    print(solution.hasCycle(head))
