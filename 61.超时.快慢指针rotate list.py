4f95284a16674ab8856352293111b1344a76892c
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-28 02:02:42 -0500

    Add files via upload

---
+++ 61.超时.快慢指针rotate list.py
@@ -0,0 +1,55 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def rotateRight(self, head, k):
+        if not head:
+            return None
+        a,b = head,head
+        while k > 0:
+            b = b.next
+            k -= 1
+            if b == None:
+                b = head
+        while b.next:
+            a = a.next
+            b = b.next
+        b.next = head
+        c = a.next
+        a.next = None
+        return c
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.rotateRight(head,2)
+    print('结果：')
+    print_linked_list(result)
