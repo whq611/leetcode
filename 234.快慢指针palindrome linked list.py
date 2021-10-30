ea0348a97a911799e59409fb4447ae4594739130
whq611 <85942669+whq611@users.noreply.github.com>
2021-10-30 03:39:45 -0500

    Add files via upload

---
+++ 234.快慢指针palindrome linked list.py
@@ -0,0 +1,60 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head):
+        
+        dummy = ListNode(-1,head)
+        a,b = dummy,dummy
+        c = []
+        while b and b.next:
+            a = a.next
+            c.append(a)
+            b = b.next.next
+        if b:
+            while a.next:
+                a = a.next
+                if a.val != c.pop().val:
+                    return False
+            return True
+        else:
+            c.pop()
+            while a.next:
+                a = a.next
+                if a.val != c.pop().val:
+                    return False
+            return True
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.isPalindrome(head)
+    print(result)
