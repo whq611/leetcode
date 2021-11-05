86566bff35a6672ad57fe5dbedaf0fb154a16ea2
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-05 03:03:46 -0500

    Add files via upload

---
+++ 剑6.（双指针)从尾到头打印列表.py
@@ -0,0 +1,53 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reversePrint(self, head):
+        if not head or not head.next:
+            return []
+        d = []
+        a,b = head,head.next
+        a.next = None
+        while b:
+            c = b.next
+            b.next = a
+            a = b
+            b = c
+        while a:
+            d.append(a.val)
+            a = a.next
+        return d
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.reversePrint(head)
+    print(result)
