70e853e933096c4532011cee2005d2b50329ac63
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-06 02:35:58 -0500

    Add files via upload

---
+++ 剑24.双指针.反转链表.py
@@ -0,0 +1,49 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        if not head:
+            return None
+        a,b = head,head.next
+        a.next = None
+        while b:
+            c = b.next
+            b.next = a
+            a = b
+            b = c
+        return a
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.reverseList(head)
+    print('结果：')
+    print_linked_list(result)
