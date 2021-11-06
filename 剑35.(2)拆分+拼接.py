c02df712fb56eb4a2f48971cabb2e24d44b8dc5d
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-06 17:17:44 -0500

    Add files via upload

---
+++ 剑35.(2)拆分+拼接.py
@@ -0,0 +1,62 @@
+class ListNode:
+    def __init__(self, val=0, next=None, random=None):
+        self.val = val
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head):
+        if not head:
+            return None
+        cur = head
+        while cur:
+            tmp = ListNode(cur.val)
+            tmp.next = cur.next
+            cur.next = tmp
+            cur = tmp.next
+        cur = head
+        while cur:
+            if cur.random:
+                cur.next.random = cur.random.next
+            cur = cur.next.next
+        cur = res = head.next
+        pre = head
+        while cur.next:
+            pre.next = pre.next.next
+            cur.next = cur.next.next
+            pre = pre.next
+            cur = cur.next
+        return res
+        
+
+
+def create_linked_list(nums):
+    if len(nums) == 0:
+        return None
+    head = ListNode(nums[0])
+    cur = head
+    for i in range(1, len(nums)):
+        cur.next = ListNode(nums[i])
+        cur = cur.next
+    return head
+
+
+def print_linked_list(list_node):
+    if list_node is None:
+        return
+
+    cur = list_node
+    while cur:
+        print(cur.val, '->', end=' ')
+        cur = cur.next
+    print('null')
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5, 6, 7]
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    head = create_linked_list(nums)
+    solution = Solution()
+    result = solution.copyRandomList(head)
+    print('结果：')
+    print_linked_list(result)
