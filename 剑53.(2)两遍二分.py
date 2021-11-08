d73903d88503219ffe1fdfedea74581e36c744fa
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-08 03:13:48 -0600

    Add files via upload

---
+++ 剑53.(2)两遍二分.py
@@ -0,0 +1,10 @@
+class Solution:
+    def search(self, nums, target):
+        def helper(tar):
+            i, j = 0, len(nums) - 1
+            while i <= j:
+                m = (i + j) // 2
+                if nums[m] <= tar: i = m + 1
+                else: j = m - 1
+            return i
+        return helper(target) - helper(target - 1)
