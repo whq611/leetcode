aa4ce6f39b35e27f3d0fd5c36cefd2473ea28dad
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-08 21:55:56 -0600

    Add files via upload

---
+++ 剑11.(1)(二分)旋转数字的最小数字.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minArray(self, numbers):
+        i,j = 0,len(numbers)-1
+        while i<j:
+            m = (i+j)//2
+            if numbers[m] > numbers[j]:
+                i = m + 1
+            elif numbers[m] < numbers[j]:
+                j = m
+            else:
+                j -= 1
+        return numbers[i]
