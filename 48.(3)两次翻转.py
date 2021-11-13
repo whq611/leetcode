bff63ef4bfd3026603171bfd625a0ec4e503f498
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-13 01:40:47 -0600

    Add files via upload

---
+++ 48.(3)两次翻转.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotate(self, matrix):
+        n = len(matrix)
+        # 水平翻转
+        for i in range(n // 2):
+            for j in range(n):
+                matrix[i][j], matrix[n - i - 1][j] = matrix[n - i - 1][j], matrix[i][j]
+        # 主对角线翻转
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
