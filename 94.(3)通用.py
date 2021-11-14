272dc90e77229b13eea36f135763e53a0ea86585
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-14 15:51:48 -0600

    Add files via upload

---
+++ 94.(3)通用.py
@@ -0,0 +1,11 @@
+class Solution:
+    def inorderTraversal(self,root):
+        stack,rst = [root],[]
+        while stack:
+            i = stack.pop()
+            if isinstance(i,TreeNode):
+                stack.extend([i.right,i.val,i.left])
+            elif isinstance(i,int):
+                rst.append(i)
+        return rst
+            
