05c91992f84da3d109349307423d5bf8131e27af
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-14 15:18:19 -0600

    Add files via upload

---
+++ 94.递归binary tree inorder traversal.py
@@ -0,0 +1,5 @@
+class Solution:
+    def inorderTraversal(self,root):
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
