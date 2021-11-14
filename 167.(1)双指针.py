fd6541f4824978eab324efc5d5991559c5af3bc9
whq611 <85942669+whq611@users.noreply.github.com>
2021-11-13 18:19:09 -0600

    Add files via upload

---
+++ 167.(1)双指针.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        low,high = 0,len(numbers)-1
+        while low<high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low+1,high+1]
+            elif total < target:
+                low += 1
+            else:
+                high -= 1
+        return [-1,-1]
