8d1e6ec9945dbd76c3620f16ee10ae0585d8c795
whq611 <85942669+whq611@users.noreply.github.com>
2022-01-16 23:29:11 -0600

    Add files via upload

---
+++ 剑48.1双指针+哈希表.py
@@ -0,0 +1,9 @@
+class Solution:
+    def length0fLongestSubstring(self,s):
+        dic, res, i = {}, 0, -1
+        for j in range(len(s)):
+            if s[j] in dic:
+                i = max(dic[s[j]],i)
+            dic[s[j]] = j
+            res = max(res,j-i)
+        return res
