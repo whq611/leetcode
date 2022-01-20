65ef34b8b661d4bfb15f41884f7241e86c2b7266
whq611 <85942669+whq611@users.noreply.github.com>
2022-01-20 02:09:45 -0600

    Add files via upload

---
+++ 剑13.3bfs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def movingCount(self, m, n, k):
+        queue, visited = [(0, 0, 0, 0)], set()
+        while queue:
+            i, j, si, sj = queue.pop(0)
+            if i >= m or j >= n or k < si + sj or (i, j) in visited: continue
+            visited.add((i,j))
+            queue.append((i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj))
+            queue.append((i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8))
+        return len(visited)
