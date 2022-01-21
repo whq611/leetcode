85dacbba88d7fa6583e51690a93f9a742c6d6c04
whq611 <85942669+whq611@users.noreply.github.com>
2022-01-20 22:26:52 -0600

    Add files via upload

---
+++ 剑34.1.dfs.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def pathSum(self,root,target):
+        res,path = [],[]
+        def recur(root,tar):
+            if not root:
+                return
+            path.append(root.val)
+            tar -= root.val
+            if tar == 0 and not root.left and not root.right:
+                res.append(list(path))
+            recur(root.left,tar)
+            recur(root.right,tar)
+            path.pop()
+        recur(root,target)
+        return res
