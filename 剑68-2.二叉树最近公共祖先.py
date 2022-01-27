9822fa6c2cdadbfac65a57d6529ab8417adbe65d
whq611 <85942669+whq611@users.noreply.github.com>
2022-01-27 02:32:32 -0600

    Add files via upload

---
+++ 剑68-2.二叉树最近公共祖先.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        if not left:
+            return right
+        if not right:
+            return left
+        return root
