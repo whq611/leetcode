f1b300082187ebc2e5e45771be2341cfe1f5a7f0
whq611 <85942669+whq611@users.noreply.github.com>
2022-05-23 23:17:52 -0500

    Add files via upload

---
+++ jian06_1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+public class jian06_1 {
+    ArrayList<Integer> tmp = new ArrayList<Integer>();
+    public int[] reversePrint(ListNode head) {
+        recur(head);
+        int[] res = new int[tmp.size()];
+        for(int i = 0; i < res.length; i++)
+            res[i] = tmp.get(i);
+        return res;
+    }
+    void recur(ListNode head) {
+        if(head == null) return;
+        recur(head.next);
+        tmp.add(head.val);
+    }
+
+
+}
