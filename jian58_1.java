da8116fe3bbdd81e64fdf68c874fb8dfbdfef1b2
whq611 <85942669+whq611@users.noreply.github.com>
2022-05-24 15:22:08 -0500

    Add files via upload

---
+++ jian58_1.java
@@ -0,0 +1,12 @@
+public class jian58_1 {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder res = new StringBuilder();
+        for(int i = n; i < s.length(); i++)
+            res.append(s.charAt(i));
+        for(int i = 0; i < n; i++)
+            res.append(s.charAt(i));
+        return res.toString();
+    }
+
+
+}
