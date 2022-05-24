f556ccc3d38bb3623b6a9925212dde961d832a39
whq611 <85942669+whq611@users.noreply.github.com>
2022-05-24 13:05:11 -0500

    Add files via upload

---
+++ jian05_1.java
@@ -0,0 +1,13 @@
+public class jian05_1 {
+    public String replaceSpace(String s) {
+        StringBuilder res = new StringBuilder();
+        for(Character c : s.toCharArray())
+        {
+            if(c == ' ') res.append("%20");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+
+
+}
