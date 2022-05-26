ed24f2fa3431b092d1d8b2f465a2d8d1db035a8c
whq611 <85942669+whq611@users.noreply.github.com>
2022-05-26 02:31:16 -0500

    Add files via upload

---
+++ 121.买卖股票的最佳时机.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in prices:
+            if i < min_price:
+                imin_price = i
+            elif i - min_price > max_profit:
+                max_profit = i - min_price
+        return max_profit
