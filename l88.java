31520f3895d824e5a95d62396dd8fab03c60523a
whq611 <85942669+whq611@users.noreply.github.com>
2022-05-29 02:15:34 -0500

    Add files via upload

---
+++ l88.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class l88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0;i<n;i++){
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
