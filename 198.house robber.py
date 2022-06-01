48a7ba36d0eee899354dc4c8efbfb2970d52bbb1
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-01 17:16:03 -0500

    Add files via upload

---
+++ 198.house robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        res = [0] * len(nums)
+        res[0] = nums[0]
+        res[1] = max(nums[0], nums[1])
+        for i in range(2,len(nums)):
+            res[i] = max(res[i-2] + nums[i], res[i-1])
+        return res[-1]
