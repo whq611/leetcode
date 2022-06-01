45216d2793f6ed000acb3755aa2a8171a9c7c595
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-01 18:39:23 -0500

    Add files via upload

---
+++ l206_1.java
@@ -0,0 +1,23 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+public class l206_1 {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        pre.next = null;
+        while(cur!=null){
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
