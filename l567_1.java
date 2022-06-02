c1c3711a0389d306e6aef70f51ebb5085d87fa0b
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-02 02:26:02 -0500

    Add files via upload

---
+++ l567_1.java
@@ -0,0 +1,48 @@
+public class l567_1 {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if (n > m) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; ++i) {
+            --cnt[s1.charAt(i) - 'a'];
+            ++cnt[s2.charAt(i) - 'a'];
+        }
+        int diff = 0;
+        for (int c : cnt) {
+            if (c != 0) {
+                ++diff;
+            }
+        }
+        if (diff == 0) {
+            return true;
+        }
+        for (int i = n; i < m; ++i) {
+            int x = s2.charAt(i) - 'a', y = s2.charAt(i - n) - 'a';
+            if (x == y) {
+                continue;
+            }
+            if (cnt[x] == 0) {
+                ++diff;
+            }
+            ++cnt[x];
+            if (cnt[x] == 0) {
+                --diff;
+            }
+            if (cnt[y] == 0) {
+                ++diff;
+            }
+            --cnt[y];
+            if (cnt[y] == 0) {
+                --diff;
+            }
+            if (diff == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
