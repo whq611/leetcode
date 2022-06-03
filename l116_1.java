8a897585d74c386b7486e428d275722a6862059a
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-03 02:41:43 -0500

    Add files via upload

---
+++ l116_1.java
@@ -0,0 +1,48 @@
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+}
+public class l116_1 {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        
+        // 从根节点开始
+        Node leftmost = root;
+        
+        while (leftmost.left != null) {
+            
+            // 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            Node head = leftmost;
+            
+            while (head != null) {
+                
+                // CONNECTION 1
+                head.left.next = head.right;
+                
+                // CONNECTION 2
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                
+                // 指针向后移动
+                head = head.next;
+            }
+            
+            // 去下一层的最左的节点
+            leftmost = leftmost.left;
+        }
+        
+        return root;
+    }
+
+}
