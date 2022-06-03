cd08879c1d0d65227ac5eb662a7d2267e11c75ee
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-03 02:36:20 -0500

    Add files via upload

---
+++ 116.2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+        # 从根节点开始
+        leftmost = root
+        
+        while leftmost.left:
+            
+            # 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            head = leftmost
+            while head:
+                
+                # CONNECTION 1
+                head.left.next = head.right
+                
+                # CONNECTION 2
+                if head.next:
+                    head.right.next = head.next.left
+                
+                # 指针向后移动
+                head = head.next
+            
+            # 去下一层的最左的节点
+            leftmost = leftmost.left
+        
+        return root 
