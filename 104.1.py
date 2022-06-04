68a7c6a12e7d9c9b66e6af3ff3efa9d0610686d5
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-03 21:16:36 -0500

    Add files via upload

---
+++ 104.1.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = collections.deque([root])
+        res = 0
+        while queue:
+            res += 1
+            l = len(queue)
+            for i in range(l):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return res
