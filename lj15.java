fa63c079d50b75c7a5a0793204c5a501082cc66a
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-12 19:46:21 -0500

    Add files via upload

---
+++ lj15.java
@@ -0,0 +1,12 @@
+public class lj15 {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n!=0){
+            n = n&(n-1);
+            res+=1;
+        }
+
+        return res;
+
+    }
+}
