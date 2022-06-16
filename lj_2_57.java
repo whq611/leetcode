9c98f3b979ccdd97c4382005c2f0784929ff076d
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-16 03:02:09 -0500

    Add files via upload

---
+++ lj_2_57.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+public class lj_2_57 {
+    public int[][] findContinuousSequence(int target) {
+        int i = 1, j = 2, s = 3;
+        List<int[]> res = new ArrayList<>();
+        while(i < j) {
+            if(s == target) {
+                int[] ans = new int[j - i + 1];
+                for(int k = i; k <= j; k++)
+                    ans[k - i] = k;
+                res.add(ans);
+            }
+            if(s >= target) {
+                s -= i;
+                i++;
+            } else {
+                j++;
+                s += j;
+            }
+        }
+        return res.toArray(new int[0][]);
+    }
+
+}
