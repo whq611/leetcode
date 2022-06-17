8c89b8acdf48d32c0677c36a762f8a1622bfba44
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-17 03:24:03 -0500

    Add files via upload

---
+++ lj31.java
@@ -0,0 +1,16 @@
+public class lj31 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for(int num : pushed) {
+            stack.push(num); // num 入栈
+            while(!stack.isEmpty() && stack.peek() == popped[i]) { // 循环判断与出栈
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+
+
+    }
+}
