55d33e29b1d13dc6607a2a86667ce77717f5c709
whq611 <85942669+whq611@users.noreply.github.com>
2022-06-21 17:45:52 -0500

    Add files via upload

---
+++ l49.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class l49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<String, List<String>>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
